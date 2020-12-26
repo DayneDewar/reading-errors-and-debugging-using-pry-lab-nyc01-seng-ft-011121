6cf5006088c7218a75785fc0660c7c2d7e91d92f
Dayne Dewar <daynedewar@yahoo.com>
2020-12-26 05:06:14 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -8,17 +8,21 @@ def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
+  binding.pry
 end
 
 def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
+     binding.pry
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
+    binding.pry
   end
 end
 
 def runner
   prompt_user
   selection(get_user_input)
+  binding.pry
 end
